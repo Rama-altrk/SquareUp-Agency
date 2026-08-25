a07e63b02f2425ee782dd9728f01510c9bf8453e
Rama-altrk <rama.altrk19@gmail.com>
2026-08-25 17:08:40 +0300

    Updating color variables

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {COLORS} from './assets/constants/colors.js'
 import './assets/styles/colors.css'
 
 createRoot(document.getElementById('root')).render(
