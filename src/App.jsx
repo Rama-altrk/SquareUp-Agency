34cc037ae1d8358535c129ad6becc2f91ca79619
Belasansalhab <belasan835@gmail.com>
2026-08-27 03:36:01 -0700

    Add new updetes

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ServicesCards from './Components/ServicesCards'
+
+function App() {
+
+  return (
+    <div className='mainContainer'>
+      <ServicesCards/>
+    </div>
+  )
+}
+
+export default App
