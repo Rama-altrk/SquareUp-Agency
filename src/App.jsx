d09350bef027f7012efc2e96b49c9e0ac9b447a9
mhd-rasheed <mamikaraoglu100@gmail.com>
2026-08-28 10:01:50 +0300

    new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import './App.css'
+import Button from './components/Button/Button'
+import CtaSection from './components/ctaSection/CtaSection'
+import WyhChooseSeciton from './components/wyhChooseSection/WyhChooseSeciton'
+
 
 function App() {
 
   return (
     <div className='mainContainer'>
+{/*       <Button 
+      name="submit"
+      width={1}
+      height={1}
+      backgroundColor="var( --grey20)"
+      color= "var( --green99)"
+      borderRadius={1}
+      fontSize={1}
+      border={1}
+      /> */}
       
+
+{/* <CtaSection/> */}
+
+{/* <WyhChooseSeciton/> */}
+
     </div>
   )
 }
