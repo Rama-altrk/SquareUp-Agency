8cf9b83139d1aeaf8a7172e853db5b91d35d0eb7
Belasansalhab <belasan835@gmail.com>
2026-08-31 05:44:24 -0700

    update

--- src/Components/ServicesCards/ServicesCards.jsx
+++ src/Components/ServicesCards/ServicesCards.jsx
@@ -7,19 +7,19 @@ const servicesData = [
     {
         id: 1,
         title: "Design",
-        description: "At SquareUp, our design team is passionate about creating beautiful, user-centric designs that captivate your audience and elevate your brand.",
+        description: "At Squareup, our design team is passionate about creating stunning, user-centric designs that captivate your audience and elevate your brand. We believe that great design is not just about aesthetics; it's about creating seamless and intuitive user experiences.",
         icon: "public/img/homeImg/DesignIcon.png" 
     },
     {
         id: 2,
         title: "Engineering",
-        description: "Our engineering team combines technical expertise with a passion for innovation to build robust, scalable, and secure digital solutions.",
+        description: "Our engineering team combines technical expertise with a passion for innovation to build robust and scalable digital solutions. We leverage the latest technologies and best practices to deliver high-performance applications tailored to your specific needs.",
         icon: "public/img/homeImg/EngineeringIcon.png"
     },
     {
         id: 3,
         title: "Project Management",
-        description: "Our experienced project management team ensures that your projects are delivered on time, within budget, and according to your specifications.",
+        description: "Our experienced project management team ensures that your projects are delivered on time, within budget, and according to your specifications. We follow industry-standard methodologies and employ effective communication and collaboration tools to keep you informed throughout the development process.",
         icon: "public/img/homeImg/ProjectManagementIcon.png"
     }
 ]
