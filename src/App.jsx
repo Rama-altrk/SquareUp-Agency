4bdddc1e4ad1158f23dc0729abde70295abe3bcc
Belasansalhab <belasan835@gmail.com>
2026-08-31 05:48:36 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <SquareUp 
           title="Thank you for your Interest in SquareUp."
           description="We would love to hear from you and discuss how we can help bring your digital ideas to life. Here are the different ways you can get in touch with us."
-          button={<button>Contact Us</button>}
+          button={<button>Start Project</button>}
 />
     </div>
   )
