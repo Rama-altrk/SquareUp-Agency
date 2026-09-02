44ae3048990a637014c41a8f2d4325e826473320
Batoul2026 <batoulh013@gmail.com>
2026-09-02 11:31:45 +0300

    finalll

--- src/components/Contactmeta/Contactmeta.jsx
+++ src/components/Contactmeta/Contactmeta.jsx
@@ -1,7 +1,8 @@
+import { COLORS } from '../../assets/constants/colors';
 import { FONTS } from '../../assets/constants/fonts'
 import './Contactmeta.css'
 import { FaFacebookF, FaTwitter, FaLinkedinIn } from 'react-icons/fa';
-import { MdEmail, MdPhone, MdLocationOn } from 'react-icons/md';
+
 
 function Contactmeta() {
   return (
@@ -16,9 +17,13 @@ function Contactmeta() {
            <div className="bh-box">
              <span className='bh-spn3' style={{...FONTS.medium}}>Stay Connected</span>
             <div className='bh-icon'>
-                <div className=' bh-i'><i class="fa-brands fa-facebook"></i> </div>
-                <div className="bh-i"><i class="fa-brands fa-twitter"></i></div>
-                <div className="bh-i"> <i class="fa-brands fa-linkedin"></i></div>
+                <div className="big"><a href="#" ><div className="bh-iface"><FaFacebookF style={{color:COLORS.grey15,fontSize:20}}/></div></a></div>
+              <div className="big"><a href="#" ><div className="bh-itwitter"><FaTwitter style={{color:COLORS.green50,fontSize:20}}/></div></a></div>
+              <div className="big">  <a href="#"><div className="bh-linkedin"><FaLinkedinIn style={{color:COLORS.grey15,fontSize:20}}/></div></a></div>
+
+
+
+
             </div>
             
            </div>
--- src/components/Socialmedia/Socialmedia.jsx
+++ src/components/Socialmedia/Socialmedia.jsx
@@ -1,18 +1,16 @@
 import { COLORS } from '../../assets/constants/colors'
 import { FONTS } from '../../assets/constants/fonts'
 import './Socialmedia.css'
+import { MdEmail, MdPhone, MdLocationOn } from 'react-icons/md';
 
 function Socialmedia() {
   return (
-    <div className='bh-container'>
-        <div className="bh-child1" style={{...FONTS .medium}}> <i class="fa-solid fa-envelope" style={{color:COLORS.green60,fontSize:26.25}}></i>hello@squareup.com</div>
-        <div className="bh-child1"style={{...FONTS .medium}}><i class="fa-solid fa-phone" style={{color:COLORS.green60,fontSize:26.25}}></i>+91 91813 23 2309</div>
-        <div className="bh-child1" style={{...FONTS .medium}}><i class="fa-solid fa-location-dot" style={{color:COLORS.green60,fontSize:26.25}}></i> Get Location</div>
-
+       <div className='bh-container'>
+          <a href="#" style={{...FONTS.medium}} className='bh-child1'> <div className="bh-Facebook"><MdEmail style={{color:COLORS.green60,fontSize:26.25}}/></div>hello@squareup.com</a>
+          <a href="#" style={{...FONTS.medium}} className='bh-child1'><div className="bh-ohone"><MdPhone style={{color:COLORS.green60,fontSize:26.25}}/></div>+91 91813 23 2309</a>
+          <a href="#" style={{...FONTS.medium}} className='bh-child1'><div className="bh-location"><MdLocationOn  style={{color:COLORS.green60,fontSize:26.25}}/></div>Get Location</a>
     </div>
+
   )
 }
-
 export default Socialmedia
-
-
