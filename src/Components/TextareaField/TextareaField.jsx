77b1ae5c31aaa8ea9720253caa2846bf86a51ee8
Rama-altrk <rama.altrk19@gmail.com>
2026-09-14 12:14:37 +0300

    create textarea as a componant

---
+++ src/Components/TextareaField/TextareaField.jsx
@@ -0,0 +1,14 @@
+import { COLORS } from '../../assets/constants/colors'
+import { FONTS } from '../../assets/constants/fonts'
+import './TextareaField.css'
+
+export default function TextareaField({className , labelName}) {
+    return (
+        <div className={`${className} rtThirdRow`}>
+            <label htmlFor="message" style={{...FONTS.medium , color: COLORS.absolutefff}}>{labelName}</label>
+            <div>
+                <textarea name="message" id="message" placeholder="Type here" style={{...FONTS.regular , color: COLORS.absolutefff}}></textarea>
+            </div>
+        </div>
+    )
+}
